92ccbd374ce126956406d719a00fcad429a744d0
vladimir gorea <vladimir.gorea@gmail.com>
2018-10-21 12:34:18 +0300

    API Interface and Adapter classes

---
+++ api_interface/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApiInterfaceConfig(AppConfig):
+    name = 'api_interface'
---
+++ api_interface/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from api_interface import adapters
+
+
+class ApiInterface(models.Model):
+    name = models.CharField(max_length=128)
+    endpoint = models.CharField(max_length=128)
+    api_user = models.CharField(max_length=256)
+    api_key = models.CharField(max_length=256)
+    adapter = models.CharField(max_length=16, choices=adapters.get_all_adapters())
+
+    def place(self, order):
+        adapter_class_ = getattr(adapters, self.adapter)
+        adapter_ = adapter_class_(self.endpoint, self.api_user, self.api_key)
+        result = adapter_.place_order(order)
+        return result
+
+    def __str__(self):
+        return self.name
